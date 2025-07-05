3eb1338f11025542b2ade2399ba8aa034860d001
Navya <navyanalam5@gmail.com>
2025-07-05 12:37:26 +0530

    Profile Service for Hotel Booking System

---
+++ src/main/java/com/navya/profileservice/ProfileRepository.java
@@ -0,0 +1,7 @@
+package com.navya.profileservice;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface ProfileRepository extends MongoRepository<Profile, String>
+{
+}
---
+++ src/test/java/com/navya/profileservice/SocialServiceApplicationTests.java
@@ -0,0 +1,12 @@
+package com.navya.profileservice;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialServiceApplicationTests {
+
+    //@Test
+    void contextLoads() {
+    }
+
+}
